ece61f8e2056ad8227f216c2d86adf942516a775
Solomon Bothwell <ssbothwell@gmail.com>
2020-02-29 12:30:00 -0800

    Updates packaging with new name

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, base, ghc-prim, hpack, integer-gmp, stdenv }:
 mkDerivation {
-  pname = "myPrelude";
+  pname = "hCatLab";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [ base ghc-prim integer-gmp ];
   libraryToolDepends = [ hpack ];
   testHaskellDepends = [ base ghc-prim integer-gmp ];
   prePatch = "hpack";
-  homepage = "https://github.com/ssbothwell/myPrelude#readme";
+  homepage = "https://github.com/ssbothwell/hcatlab#readme";
   license = stdenv.lib.licenses.bsd3;
 }
